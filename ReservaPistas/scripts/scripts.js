f1454ba7f99967b6987a3bb61ae7eab70ab96830
alumnofp <alumnofp@FPB06.CALASANZSALAMANCA.local>
2023-12-05 13:13:06 +0100

    Mamada historica

--- ReservaPistas/scripts/scripts.js
+++ ReservaPistas/scripts/scripts.js
@@ -1,5 +1,6 @@
 let datosReserva = {};
 
+
 function nuevoCalendario() {
     let deporte = document.getElementById('deporte').value;
 
@@ -91,16 +92,16 @@ function generarCalendario() {
 }
 function contarDiasLibres() {
     let casillas = document.querySelectorAll('#divMes td');
-    let diasDisponibles = 0;
+    let diasLibres = 0;
 
     casillas.forEach(casilla => {
         if (casilla.textContent.trim() !== '' && !casilla.classList.contains('reservada') && !casilla.classList.contains('fondoViolet')) {
-            diasDisponibles++;
+            diasLibres++;
         }
     });
 
     
-    return diasDisponibles;
+    return diasLibres;
 }
 
 function mostrarDiasLibres() {
@@ -110,18 +111,74 @@ function mostrarDiasLibres() {
         const diasLibres = contarDiasLibres();
         const mensaje = `Quedan ${diasLibres} días libres`;
         divAvisos.innerHTML = `<p>${mensaje}</p>`;
+        if(diasLibres == 0) {
+            alert('Ya no quedan más días disponibles');
+        }
     }, 3000);
+}
+function verificarDiasLibres() {
+    const diasLibres = contarDiasLibres();
 
-    if(diasLibres == 0) {
+    if (diasLibres === 0) {
         alert('Ya no quedan más días disponibles');
+        limpiarPanel();
     }
 }
 
-function asignarReserva(casilla){
-
+function cambiarColor(casilla) {
     let deporte = document.getElementById('deporte').value;
-    
+
+    if (!deporte) {
+        mostrarAviso('Debes introducir primero un deporte.');
+        return;
+    }
+
+    if (casilla.classList.contains('fondoViolet') || casilla.classList.contains('casillaSeleccionada')) {
+        mostrarAviso('Esta casilla no se puede seleccionar.');
+        return;
+    }
+
+    if (casilla.textContent !== '') {
+        if (document.getElementById('divMes').innerHTML !== '') {
+            casilla.classList.add('reservada');
+            verificarDiasLibres();
+        } else {
+            mostrarAviso('Debes generar primero el calendario.');
+        }
+    }
+}
 
 
+
+function limpiarPanel() {
+    const divMes = document.getElementById('divMes');
+    const divAvisos = document.getElementById('divAvisos');
+    const deporte = document.getElementById('deporte');
+
+    divMes.innerHTML = '';
+    divAvisos.innerHTML = '';
+    deporte.value = '';
+}
+
+window.onload = () =>{
+const encabezado = document.querySelector('h1');
+let colorOriginal = true; 
+
+
+function cambiarColorEncabezado() {
+    if (colorOriginal) {
+        encabezado.style.color = 'green'; 
+        colorOriginal = false;
+    } else {
+        encabezado.style.color = ''; 
+        colorOriginal = true;
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'x' || event.key === 'X') {
+        cambiarColorEncabezado();
+    }
+});
 }
 
